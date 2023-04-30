f35632b307b3435f00278d55a7c93957c5472ef0
michaelplatt07 <michael.platt.07@gmail.com>
2023-04-29 19:36:42 -0600

    Char at syntax check (#39)
    * charAt syntax checking
    * Now checking the method parameters for the `charAt` call.
    * Created a helper method that will return a list of all primitives
      being used in an AST starting from a particular node
    * Tests to ensure `charAt` raises exceptions appropriately.
    * Updated the test_programs to allow for skipping of certain files to
      make reading output easier.
    * Added new integration test for the `charAt` keyword.
    
    * charAt syntax checking
    * Fixed sample program and output that should have been printing just a
      single char.
    
    ---------
    
    Co-authored-by: michaelplatt07 <michael@lugia.lugia>

--- tests/test_programs.py
+++ tests/test_programs.py
@@ -24,17 +24,20 @@ def compare_expected_output_to_program_output(expected_results_dir, input_file_d
             results[program.split(".")[0]] = program_results.readlines()
             to_be_compare_results.add(program.split(".")[0])
 
+    to_be_compare_results = to_be_compare_results - set(ignore_list)
     for program in os.listdir(expected_results_dir):
         output_file_name = os.getcwd() + "/expected_outputs/" + program
         with open(output_file_name, "r") as expected_results:
             key = program.split(".")[0].replace("_expected_output", "")
-            expected_lines = expected_results.readlines()
-            compared_results.add(key)
-            # Don't want to print the massive rule_110 results right now.
-            if display_compare and key != "rule_110":
-                build_output_table_row(
-                    output_file_name.split("/")[-1], program.replace("_expected_output", "_res"), expected_lines, results[key])
-            assert results[key] == expected_lines, f"Output not the same as expected for {key} | Results = {results[key]} | Expected = {expected_results.readlines()}"
+            if key not in ignore_list:
+                expected_lines = expected_results.readlines()
+                compared_results.add(key)
+                # Don't want to print the massive rule_110 results right now.
+                if display_compare and key != "rule_110":
+                    build_output_table_row(
+                        output_file_name.split("/")[-1], program.replace("_expected_output", "_res"), expected_lines, results[key])
+                assert results[
+                    key] == expected_lines, f"Output not the same as expected for {key} | Results = {results[key]} | Expected = {expected_results.readlines()}"
 
     if missing_compares := to_be_compare_results - compared_results:
         raise Exception(
@@ -101,19 +104,16 @@ def build_expected_outputs(sample_program_dir):
 
 def build_new_only(expected_results_dir, sample_program_dir):
     # Build list of results that already exist
-    # breakpoint()
     expected_results_file_list = set()
     for program in os.listdir(expected_results_dir):
         expected_results_file_list.add(program.split(
             ".")[0].replace("_expected_output", ""))
 
-    # breakpoint()
     # Build list of all files in the sample file dir.
     sample_file_list = set()
     for program in os.listdir(sample_program_dir):
         sample_file_list.add(program.split(".")[0])
 
-    # breakpoint()
     if results_to_make := sample_file_list - expected_results_file_list:
         for program in results_to_make:
             output_file_name = os.getcwd() + "/expected_outputs/" + \
@@ -135,11 +135,15 @@ if __name__ == "__main__":
                             help="Switch to recreate all the expected outputs.")
     arg_parser.add_argument("--create-new-only", action="store_true",
                             help="Switch to only the results for newly added or missing tests.")
+    arg_parser.add_argument("--ignore", nargs="+",
+                            help="List of tests to ignore.")
+
     args = arg_parser.parse_args()
 
     new_only = args.create_new_only
     recreate_expected_outputs = args.recreate_all
     display_compare = args.display_compare
+    ignore_list = args.ignore or []
 
     try:
         if recreate_expected_outputs:
